d086e213ed28f32071699dd889d1124562bfc6fb
lxxx <1318753541@qq.com>
2024-11-15 16:01:44 +0800

    chore(db): 添加数据库生成和迁移脚本
    - 在根 package.json 中添加 db:generate 和 db:migrate 脚本
    - 在 db 包中添加 drizzle:generate 和 drizzle:migrate 脚本
    - 更新 db 包的依赖，添加 @types/pg 和 tsx

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import type { Config } from "drizzle-kit";
+
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../../apps/server/.env") });
+
+console.log("process.env.DATABASE_URL: ", process.env.DATABASE_URL);
+
+export default {
+  schema: "../schema/src/schema/*",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL || "",
+  },
+} satisfies Config;
