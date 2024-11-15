bc7edd3808f90625a24b19d0f012896cb916953b
lxxx <1318753541@qq.com>
2024-11-15 16:55:25 +0800

    feat(client): 集成 Nuxt UI 组件库
    - 在 nuxt.config.ts 中添加 @nuxt/ui 模块
    - 在 package.json 中添加 @nuxt/ui 依赖
    - 在首页添加 UAlert 和 UIcon 组件示例

--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -4,12 +4,16 @@ const route = useRoute();
 
 <template>
   <div>
+    <UAlert
+    icon="i-heroicons-command-line"
+    color="primary"
+    variant="solid"
+    title="Heads up!"
+    description="You can add components to your app using the cli."
+  />
+    <UIcon name="i-mdi:account" class="w-5 h-5" />
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
-    <a
-      href="https://nuxt.com/docs/getting-started/routing"
-      target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
 </template>
